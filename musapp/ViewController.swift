a0d099607df54eb14df847e7ad25e21e8f1bea94
ilnur.shafigullin <lexboss777@gmail.com>
2023-10-30 19:29:44 +0300

    added empty project

---
+++ musapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  musapp
+//
+//  Created by imac on 30.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
