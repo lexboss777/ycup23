5de3ec6e2519c4d0660db967f9afb73094b0d599
Ilnur Shafigullin <lexboss777@gmail.com>
2023-12-02 11:21:12 +0600

    very simple anim

---
+++ musapp/VizualizeControls/ThreeDots.swift
@@ -0,0 +1,57 @@
+//
+//  ThreeDots.swift
+//  musapp
+//
+//  Created by Ilnur Shafigullin on 02.12.2023.
+//
+
+import Foundation
+import UIKit
+
+class ThreeDots : UIView {
+    var d1: UIView!
+    var d2: UIView!
+    var d3: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setup()
+    }
+
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+
+    private func setup() {
+
+        backgroundColor = .clear
+        
+        d1 = getDotView()
+        d1.frame = CGRect(0, 0, 27.35, 27.35)
+        d1.layer.cornerRadius = d1.frame.width / 2
+        
+        d2 = getDotView()
+        d2.frame = CGRect(51, 20, 27.35, 27.35)
+        d2.layer.cornerRadius = d2.frame.width / 2
+        
+        d3 = getDotView()
+        d3.frame = CGRect(11, 58, 27.35, 27.35)
+        d3.layer.cornerRadius = d3.frame.width / 2
+    }
+    
+    func getDotView() -> UIView {
+        var v = UIView()
+        v.backgroundColor = UIColor(0x5A50E2)
+        addSubview(v)
+        return v
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    override func sizeToFit() {
+        
+    }
+}
--- musapp/VizualizeViewController.swift
+++ musapp/VizualizeViewController.swift
@@ -5,9 +5,31 @@ import AudioKit
 import AVFoundation
 
 class VizualizeViewController: UIViewController {
+    
+    var threeDots: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .cyan
+        view.backgroundColor = .black
+        
+        threeDots = ThreeDots()
+        view.addSubview(threeDots)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        threeDots.sizeToFit()
+        
+        UIView.animate(withDuration: 5, delay: TimeInterval(0), options: [.repeat, .curveEaseIn], animations: { [self] () -> Void in
+            
+            let randomX = CGFloat.random(in: 14 ... 200)
+            let randomY = CGFloat.random(in: 20 ... 600)
+            
+            threeDots.center = CGPoint(x: self.threeDots.frame.origin.x + randomX, y: threeDots.frame.origin.y + randomY)
+            self.view.layoutIfNeeded()
+        }, completion: { finished in
+        })
     }
 }
