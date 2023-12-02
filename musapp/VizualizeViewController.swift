152547f57dd8d75c6d72db69d604ad4a0473b8d1
Ilnur Shafigullin <lexboss777@gmail.com>
2023-12-02 11:37:05 +0600

    spiral is animating

--- musapp/VizualizeViewController.swift
+++ musapp/VizualizeViewController.swift
@@ -7,6 +7,7 @@ import AVFoundation
 class VizualizeViewController: UIViewController {
     
     var threeDots: UIView!
+    var spiral: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,13 +16,27 @@ class VizualizeViewController: UIViewController {
         
         threeDots = ThreeDots()
         view.addSubview(threeDots)
+        animateThreeDots()
+        
+        spiral = UIImageView(image: UIImage(named: "spiral")!)
+        view.addSubview(spiral)
+        animateSpiral()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
+        let w = view.frame.width
+        let h = view.frame.height
+        
         threeDots.sizeToFit()
         
+        if spiral != nil {
+            spiral.move(0.2 * w, 0.6 * h)
+        }
+    }
+    
+    func animateThreeDots() {
         UIView.animate(withDuration: 5, delay: TimeInterval(0), options: [.repeat, .curveEaseIn], animations: { [self] () -> Void in
             
             let randomX = CGFloat.random(in: 14 ... 200)
@@ -31,5 +46,16 @@ class VizualizeViewController: UIViewController {
             self.view.layoutIfNeeded()
         }, completion: { finished in
         })
+    }
+    
+    func animateSpiral() {
+        let duration: CFTimeInterval = 3
+        let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        rotateAnimation.fromValue = 0.0
+        rotateAnimation.toValue = CGFloat(Double.pi * 2)
+        rotateAnimation.isRemovedOnCompletion = false
+        rotateAnimation.duration = duration
+        rotateAnimation.repeatCount=Float.infinity
+        spiral.layer.add(rotateAnimation, forKey: nil)
     }
 }
